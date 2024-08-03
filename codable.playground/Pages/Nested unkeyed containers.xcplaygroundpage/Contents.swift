42df445cd5737a8bf4fd6bebbdd9710aed9a1fa0
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-08-04 01:58:35 +0200

    nested unkeyed container

--- codable.playground/Pages/Nested unkeyed containers.xcplaygroundpage/Contents.swift
+++ codable.playground/Pages/Nested unkeyed containers.xcplaygroundpage/Contents.swift
@@ -1,5 +1,20 @@
 import Foundation
 
+/*
+ Resulting JSON representation
+ 
+ {
+   "name" : "Teddy Bear",
+   "label" : [
+     "teddy bear",
+     "TEDDY BEAR",
+     "Teddy Bear"
+   ]
+ }
+
+ */
+
+
 let encoder = JSONEncoder()
 let decoder = JSONDecoder()
 
@@ -10,8 +25,34 @@ struct Toy: Encodable {
     enum CodingKeys: CodingKey {
         case name, label
     }
+    
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.name, forKey: .name)
+        var labelContainer = container.nestedUnkeyedContainer(forKey: .label)
+        
+        try labelContainer.encode(name.lowercased())
+        try labelContainer.encode(name.uppercased())
+        try labelContainer.encode(name)
+    }
 }
+
+extension Toy: Decodable {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        var labelContainer = try container.nestedUnkeyedContainer(forKey: .label)
+        var labelName = ""
+        while !labelContainer.isAtEnd {
+            labelName = try labelContainer.decode(String.self)
+        }
+        label = labelName
+    }
+}
+
 
 let toy = Toy(name: "Teddy Bear", label: "Teddy Bear")
 let data = try encoder.encode(toy)
 let string = String(data: data, encoding: .utf8)!
+
+let sameToy = try decoder.decode(Toy.self, from: data)
