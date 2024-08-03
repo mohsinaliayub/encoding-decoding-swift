6c182db1494dbb3d0e2a3b919f2a35d90c82ed75
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-08-04 00:12:36 +0200

    encode and decode nested types

--- codable.playground/Pages/Nested types.xcplaygroundpage/Contents.swift
+++ codable.playground/Pages/Nested types.xcplaygroundpage/Contents.swift
@@ -15,3 +15,12 @@ let employee = Employee(name: "John Appleseed", id: 7, favoriteToy: toy)
 
 let encoder = JSONEncoder()
 let decoder = JSONDecoder()
+
+let data = try encoder.encode(employee)
+let string = String(data: data, encoding: .utf8)!
+print(string)
+
+
+let sameEmployee = try decoder.decode(Employee.self, from: data)
+print(sameEmployee)
+print(employee)
