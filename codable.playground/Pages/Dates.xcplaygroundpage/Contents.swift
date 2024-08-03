e3034f397367a381bf74408e4612d01f800be6ed
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-08-04 01:28:07 +0200

    encode/decode custom date representation

--- codable.playground/Pages/Dates.xcplaygroundpage/Contents.swift
+++ codable.playground/Pages/Dates.xcplaygroundpage/Contents.swift
@@ -1,11 +1,33 @@
 import Foundation
 
+/*
+ Required JSON.
+ {
+   "id" : 7,
+   "name" : "John Appleseed",
+   "birthday" : "29-05-2019",
+   "toy" : {
+     "name" : "Teddy Bear"
+   }
+ }
+ */
+
+extension DateFormatter {
+    static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd-MM-yyyy"
+        return formatter
+    }()
+}
+
 struct Toy: Codable {
     var name: String
 }
 
 let encoder = JSONEncoder()
+encoder.dateEncodingStrategy = .formatted(.dateFormatter)
 let decoder = JSONDecoder()
+decoder.dateDecodingStrategy = .formatted(.dateFormatter)
 
 struct Employee: Codable {
     var name: String
@@ -20,3 +42,4 @@ let employee = Employee(name: "John Appleseed", id: 7, birthday: Date(), toy: to
 let dateData = try encoder.encode(employee)
 let dateString = String(data: dateData, encoding: .utf8)!
 let sameEmployee = try decoder.decode(Employee.self, from: dateData)
+
