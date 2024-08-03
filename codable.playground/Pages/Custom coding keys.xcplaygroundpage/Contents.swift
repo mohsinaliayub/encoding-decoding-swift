020f1c8452429231eaf89aff6045bc017a725afd
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-08-04 00:21:18 +0200

    use custom coding keys for encoding/decoding

--- codable.playground/Pages/Custom coding keys.xcplaygroundpage/Contents.swift
+++ codable.playground/Pages/Custom coding keys.xcplaygroundpage/Contents.swift
@@ -8,6 +8,10 @@ struct Employee: Codable {
     var name: String
     var id: Int
     var favoriteToy: Toy
+    
+    enum CodingKeys: String, CodingKey {
+        case name, id, favoriteToy = "gift"
+    }
 }
 
 let toy = Toy(name: "Teddy Bear")
@@ -19,3 +23,4 @@ let decoder = JSONDecoder()
 let data = try encoder.encode(employee)
 let string = String(data: data, encoding: .utf8)!
 let sameEmployee = try decoder.decode(Employee.self, from: data)
+
