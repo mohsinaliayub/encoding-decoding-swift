58326ca75be1496c94501c4c876bc4ed0b806c78
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-08-04 00:36:40 +0200

    encode/decode models to and from flat JSON hierarchy

--- codable.playground/Pages/Keyed containers.xcplaygroundpage/Contents.swift
+++ codable.playground/Pages/Keyed containers.xcplaygroundpage/Contents.swift
@@ -8,11 +8,26 @@ struct Employee: Encodable {
     var name: String
     var id: Int
     var favoriteToy: Toy
+    
+    enum CodingKeys: CodingKey {
+        case id, name, gift
+    }
+    
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        try container.encode(id, forKey: .id)
+        try container.encode(favoriteToy.name, forKey: .gift)
+    }
 }
 
 extension Employee: Decodable {
     init(from decoder: Decoder) throws {
-        fatalError("To do")
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        id = try container.decode(Int.self, forKey: .id)
+        let gift = try container.decode(String.self, forKey: .gift)
+        favoriteToy = Toy(name: gift)
     }
 }
 
@@ -24,4 +39,6 @@ let decoder = JSONDecoder()
 
 let data = try encoder.encode(employee)
 let string = String(data: data, encoding: .utf8)!
+
+let sameEmployee = try decoder.decode(Employee.self, from: data)
 
